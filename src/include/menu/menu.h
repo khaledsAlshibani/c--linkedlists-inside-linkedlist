97fc8ad51144bc01b1d9821b2355d52cc0f3f157
Khaled Alshibani <alshibanikhaleds@gmail.com>
2024-03-04 21:08:22 +0300

    fix include and other improvements

--- src/include/menu/menu.h
+++ src/include/menu/menu.h
@@ -1,75 +1,82 @@
 #pragma once
-#include<iostream>
+#include <iostream>
+#include "../LinkedListProcessor/LinkedListProcessor.h"
 #include "../LineLinkedList/LineLinkedList.h"
 #include "../MasterLinkedList/MasterLinkedList.h"
 using namespace std;
-class menu{
-    public:
-    void displayMenu(){
-    LinkedListProcessor processor("assets/data.txt");
-    processor.processFile();
-    MasterLinkedList masterList = processor.getMasterList();
+class Menu
+{
+public:
+    void displayMenu()
+    {
+        LinkedListProcessor processor("assets/data.txt");
+        processor.processFile();
+        MasterLinkedList masterList = processor.getMasterList();
 
-       // MasterLinkedList masterList;
+        // MasterLinkedList masterList;
         LineLinkedList lineLinked;
         int num_oper;
         int indx;
         string word;
 
-        do{
-        
-        cout<<"1- For display the file\n";
-        cout<<"2- For display specific line\n";
-        cout<<"3- For display number of line in the file\n";
-        cout<<"4- For display number of letter in the file\n";
-        cout<<"5- For display number of letter in specific line\n";
-        cout<<"6- For search for word in the file\n";
-        cout<<"7- For exit\n";
-        cout<<"Enter number of operation :";
-        cin>>num_oper;
-        cout<<"\n======================================================\n\n";
-
-        switch (num_oper)
+        do
         {
-        case 1:
-        masterList.display();
-        break;
+            cout << "1- For display the file content\n";
+            cout << "2- For display a specific line by its number\n";
+            cout << "3- For display the fill number of lines in the file\n";
+            cout << "4- For display the full number of letters in the file\n";
+            cout << "5- For display number of letters in a specific line\n";
+            cout << "6- For search for a word in the file\n";
+            cout << "7- For exit\n";
+            cout << "Enter the operation number:";
 
-        case 2:{
-        cout<<"Enter number of line :";
-        cin>>indx;
-        masterList.displayByLineIndex(indx);
-        }
-        break;
+            cin >> num_oper;
+            
+            cout << "\n======================================================\n\n";
 
-        case 3:
-        cout<<masterList.getLinesCount()<<endl;
-        break;
+            switch (num_oper)
+            {
+            case 1:
+                masterList.display();
+                break;
 
-        case 4:
-        cout<<lineLinked.getLettersCount()<<"\n";
-        break;
-
-        case 5:{
-        cout<<"Enter number of line :";
-        cin>>indx;
-        cout<<lineLinked.getLettersCountByWordIndex(indx);
-        }
-        break;
-
-        case 6:
-        cout<<"Enter the word you want to search :";
-        cin>>word;
-        lineLinked.searchByWord(word);
-        break;
-
-        case 7:
-        exit(0);
-       
-        default:
+            case 2:
+            {
+                cout << "Enter number of line :";
+                cin >> indx;
+                masterList.displayByLineIndex(indx);
+            }
             break;
-        }
-        }while(num_oper>0&&num_oper<8);
+
+            case 3:
+                cout << masterList.getLinesCount() << endl;
+                break;
+
+            case 4:
+                cout << lineLinked.getLettersCount() << "\n";
+                break;
+
+            case 5:
+            {
+                cout << "Enter number of line :";
+                cin >> indx;
+                cout << lineLinked.getLettersCountByWordIndex(indx);
+            }
+            break;
+
+            case 6:
+                cout << "Enter the word you want to search :";
+                cin >> word;
+                lineLinked.searchByWord(word);
+                break;
+
+            case 7:
+                exit(0);
+
+            default:
+                break;
+            }
+        } while (num_oper > 0 && num_oper < 8);
     }
 
     // void creatFile(){
@@ -77,5 +84,4 @@ class menu{
     // processor.processFile();
     // MasterLinkedList masterList = processor.getMasterList();
     // }
-    
 };
